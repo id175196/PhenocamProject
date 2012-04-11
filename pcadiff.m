be0844a2837131c1b4e7029d50692f96fa7ad1e5
Dmitri Ilushin <dilushin@college.harvard.edu>
2012-04-11 15:08:11 -0400

    made accurate pca thresholding technique for sky/panel vs vegetation

--- pcadiff.m
+++ pcadiff.m
@@ -1,10 +1,22 @@
-function [coeff,score,latent,tsquare, mask] = pca(img)
+function [coeff,score,latent,tsquare] = pcadiff(img,mask)
 
-mask = img(:,:,:,1);
 r1 = zeros(size(mask,1)*size(mask,2),size(img,4));
 %r = zeros(size(mask,1)*size(mask,2),size(mask,3)*size(img,4));
-for n=1:size(img,4);
-    imgtemp = img(:,:,:,n);
+for n=1:size(img,4) - 1;
+    imgtemp = imabsdiff(img(:,:,:,n),img(:,:,:,n+1));
+    redtemp = imgtemp(:,:,1);
+    greentemp = imgtemp(:,:,2);
+    bluetemp = imgtemp(:,:,3);
+    redtemp(mask == 1) = 0;
+    greentemp(mask == 1) = 0;
+    bluetemp(mask == 1) = 0;
+    
+    imgtemp(:,:,1) = redtemp;
+    imgtemp(:,:,2) = greentemp;
+    imgtemp(:,:,3) = bluetemp;
+    
+    figure()
+    imagesc(imgtemp/max(max(max(imgtemp))));
     pmat = zeros(size(r1,1),size(mask,3));
     for m = 1:size(mask,3);
         pmat(:,m) = reshape(imgtemp(:,:,m),size(imgtemp,1)*size(imgtemp,2),1);
@@ -24,11 +36,7 @@ end
 %testing this comment works.
 %merges should come up here.
 [coeff,score,latent,tsquare] = princomp(r1);
-tempr = reshape(score(:,1),size(mask,1),size(mask,2));
-m2 = max(max(tempr));
-m1 = min(min(tempr));
-mask = ones(size(tempr,1),size(tempr,2));
-mask(tempr < m1 + .6*(m2-m1)) = 0;
+realr = reshape(score(:,1),size(mask,1),size(mask,2));
 figure
-imagesc(mask);
+imagesc(realr);
 end
